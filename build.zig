0b253e5c585cfd77518df21d6ba1a70b9bdb9f89
FalsePattern <me@falsepattern.com>
2025-08-06 22:27:15 +0200

    fix file reference

--- build.zig
+++ build.zig
@@ -1,8 +1,9 @@
 const std = @import("std");
 const zigglgen = @import("zigglgen");
 pub fn createGLWrapperModule(b: *std.Build, gl_bindings_module: *std.Build.Module) *std.Build.Module {
+    const self_dep = b.dependencyFromBuildZig(@This(), .{});
     return b.createModule(.{
-        .root_source_file = b.path("src/root.zig"),
+        .root_source_file = self_dep.path("src/root.zig"),
         .imports = &.{
             .{ .name = "bindings", .module = gl_bindings_module },
         }
